ca3a17608fcdc66555a383489cd80d808bcb78e8
supplient <437566830@qq.com>
2020-03-24 14:46:38 +0800

    填完了CharEmbedder大多数方法 concat因为会影响到后面的SIF，先放着不做。

--- char_emb.py
+++ char_emb.py
@@ -1,22 +1,27 @@
+from driver_amount import addh
 import config
+import keras
+import keras_bert
 
 class CharEmbedder:
-    def __init__(self):
-        import keras
-        import keras_bert
-        from driver_amount import addh
+    methods = [
+        "emb_layer",
+        "second_to_last_layer",
+        "last_layer",
+        "sum_all",
+        "sum_last_four",
+        # TODO "concat_last_four"
+    ]
 
-        bert_model, bert_model_config = keras_bert.build_model_from_config(
+    def __init__(self):
+        self.bert_model, bert_model_config = keras_bert.build_model_from_config(
             addh + config.BERT_CONFIG_PATH,
             trainable=False
         )
-        output = bert_model.get_layer("Embedding-Norm").output
-
-        self.model = keras.models.Model(bert_model.input, output)
-        self.model.load_weights(addh + config.MODEL_PATH, by_name=True)
         self.vocab = keras_bert.load_vocabulary(addh + config.BERT_VOCAB_PATH)
+        self.model = None
 
-    def embed(self, char_seqs):
+    def embed(self, char_seqs, method_name="emb_layer"):
         import preprocess
 
         # Preprocess for model predicting
@@ -32,34 +37,100 @@ class CharEmbedder:
             len(token_id_seqs[0])
         )
 
+        # Select Method to Build Model
+        method = getattr(
+            CharEmbedder,
+            "_method_" + method_name,
+            None
+            )
+        if method == None:
+            raise Exception("Invalid method name: " + method_name)
+        method(self)
+
         # Model predicting
         predict_res = self.model.predict(
             [token_id_seqs, segment_seqs],
             batch_size=4
         )
 
-        # Remove [CLS]
-        #       There is no need to remove [SEP], since the origin length can be obtained from char_seqs
+        # Remove [CLS] & [SEP]
         emb_seqs = []
         for i in range(len(predict_res)):
-            emb_seq = predict_res[i][1:]
+            emb_seq = predict_res[i][1:-2]
             emb_seqs.append(emb_seq)
 
         return emb_seqs
 
+
+
+    def _method_single_layer(self, layer_name):
+        # Build Model
+        output = self.bert_model.get_layer(layer_name).output
+        self.model = keras.models.Model(self.bert_model.input, output)
+        self.model.load_weights(addh + config.MODEL_PATH, by_name=True)
+
+    def _method_emb_layer(self):
+        self._method_single_layer(
+            "Embedding-Norm"
+        )
+
+    def _method_second_to_last_layer(self):
+        self._method_single_layer(
+            "Encoder-11-FeedForward-Norm"
+        )
+
+    def _method_last_layer(self):
+        self._method_single_layer(
+            "Encoder-12-FeedForward-Norm"
+        )
+
+    def _method_sum_layers(self, layers_name):
+        # Build Model
+        middle_outputs = []
+        for layer_name in layers_name:
+            output = self.bert_model.get_layer(layer_name).output
+            middle_outputs.append(output)
+        output = keras.layers.Add()(middle_outputs)
+        self.model = keras.models.Model(self.bert_model.input, output)
+        self.model.load_weights(addh + config.MODEL_PATH, by_name=True)
+    
+    def _method_sum_last_four(self):
+        layers = []
+        for i in range(9, 13):
+            layers.append("Encoder-%i-FeedForward-Norm" % i)
+        self._method_sum_layers(
+            layers
+        )
+
+    def _method_sum_all(self):
+        layers = []
+        for i in range(1, 13):
+            layers.append("Encoder-%i-FeedForward-Norm" % i)
+        self._method_sum_layers(
+            layers
+        )
+
+
+
     @classmethod
     def test(cls):
-        a = ["给予生以希望", "赐予死以冥道"]
-        b = ["将希望交给生", "将冥道交给死"]
+        import numpy as np
+
+        a = [
+            "给予生以希望", 
+            "赐予死以冥道",
+            "将希望交给生",
+            "将冥道交给死"
+        ]
         from preprocess import str2seq
         a = [str2seq(s) for s in a]
-        b = [str2seq(s) for s in b]
 
         embedder = CharEmbedder()
-        a_emb = embedder.embed(a)
-        b_emb = embedder.embed(b)
-        print(a_emb)
-        print(b_emb)
+        for method in CharEmbedder.methods:
+            a_emb = embedder.embed(a, method)
+            print(method + ": ")
+            p = np.array(a_emb)
+            print(p.shape)
 
 
 if __name__ == "__main__":
