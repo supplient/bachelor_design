6d8038283ac23834caaba669aab5ed35a61bed0a
supplient <437566830@qq.com>
2020-03-28 09:10:30 +0800

    Make a train notebook for sif's alpha

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ EQUAL_TRAIN_REC_PATH = "/Graduation/Model/equal_train_rec.json" # To move
 
 # log file path
 EQUAL_TRAIN_REP_PATH = "/Graduation/Log/equal_train_report.txt"
+EQUAL_SIF_TRAIN_REC_PATH = "/Graduation/Log/equal_sif_train_rec.json"
 
 # train & test common config
     # Configs which are specialized 
