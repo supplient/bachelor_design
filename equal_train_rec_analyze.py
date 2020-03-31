bff3c1b013a55fb7216857d5a85d266b6737f50f
supplient <437566830@qq.com>
2020-03-31 08:43:38 +0800

    Add csv output for convenience.

--- equal_train_rec_analyze.py
+++ equal_train_rec_analyze.py
@@ -25,8 +25,9 @@ def calMetrics(train_rec):
             rec["recall"] = r
             rec["F1"] = f1
 
-def displayTable(train_rec, output_file=None):
+def displayTable(train_rec, output_file=None, output_table=None, precision=2):
     rep = ""
+    table_rows = []
 
     # Get methods' name
     row_labels = [x for x in train_rec.keys()]
@@ -37,7 +38,8 @@ def displayTable(train_rec, output_file=None):
     header.extend(column_labels)
     sub_row_names = train_rec[row_labels[0]][column_labels[0]].keys()
     for row_label in row_labels:
-        header[0] = row_label
+        header[0] = "[%s]" % row_label
+        table_rows.append(header.copy())
         t = PrettyTable(
             field_names=header,
             header=True,
@@ -46,14 +48,17 @@ def displayTable(train_rec, output_file=None):
             row = [row_name]
             for column_label in column_labels:
                 row.append(
-                    round(train_rec[row_label][column_label][row_name], 2)
+                    round(train_rec[row_label][column_label][row_name], precision)
                 )
             t.add_row(row)
+            table_rows.append(row)
         rep += str(t) + "\n\n"
+        table_rows.append([None])
 
     # Then, display a total table
     rep += "Total: " + "\n"
     header[0] = "F1"
+    table_rows.append(header.copy())
     t = PrettyTable(
         field_names=header,
         header=True
@@ -62,17 +67,23 @@ def displayTable(train_rec, output_file=None):
         row = [row_label]
         for column_label in column_labels:
             row.append(
-                round(train_rec[row_label][column_label]["F1"], 2)
+                round(train_rec[row_label][column_label]["F1"], precision)
             )
         t.add_row(row)
+        table_rows.append(row)
     rep += str(t)
 
     if output_file:
         with open(output_file, "w") as fd:
             fd.write(rep)
+    if output_table:
+        import csv
+        with open(output_table, "w", newline='') as fd:
+            writer = csv.writer(fd)
+            writer.writerows(table_rows)
     print(rep)
 
-def makeTrainRep(train_rec_path, train_rep_path):
+def makeTrainRep(train_rec_path, train_rep_path, train_table_path=None, precision=2):
     rep = ""
 
     # Load train record
@@ -84,15 +95,22 @@ def makeTrainRep(train_rec_path, train_rep_path):
     calMetrics(train_rec)
 
     # Display tables
-    displayTable(train_rec, train_rep_path)
+    displayTable(
+        train_rec, 
+        output_file=train_rep_path, 
+        output_table=train_table_path,
+        precision=precision,
+    )
 
 if __name__ == "__main__":
     from driver_amount import addh
-    # makeTrainRep(
-        # addh + config.EQUAL_TRAIN_REC_PATH,
-        # addh + config.EQUAL_TRAIN_REP_PATH
-    # )
     makeTrainRep(
-        addh + config.EQUAL_SIF_TRAIN_REC_PATH,
-        addh + config.EQUAL_SIF_TRAIN_REP_PATH
+        addh + config.EQUAL_TRAIN_REC_PATH,
+        addh + config.EQUAL_TRAIN_REP_PATH,
+        addh + config.EQUAL_TRAIN_TABLE_PATH,
     )
+    # makeTrainRep(
+        # addh + config.EQUAL_SIF_TRAIN_REC_PATH,
+        # addh + config.EQUAL_SIF_TRAIN_REP_PATH,
+        # 4
+    # )
