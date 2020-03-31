97eaf6b2b4a0d58116a9b5194901aeafefa04099
supplient <437566830@qq.com>
2020-03-31 08:44:30 +0800

    修改了检查点的存取模式，方便后续分析，并适应cut_and_tag的新增

--- config.py
+++ config.py
@@ -9,15 +9,16 @@ BERT_CONFIG_PATH = BERT_PATH + "/bert_config.json"
 BERT_CHECKPOINT_PATH = BERT_PATH + "/bert_model.ckpt"
 
 # generated file path
-TAG_VOCAB_PATH = "/Graduation/Model/tag_vocab.pkl"
+TAG_VOCAB_PATH = "/Graduation/Model/tag_vocab.json"
 MODEL_PATH = "/Graduation/Model/model.h5"
-TRAIN_LOG_PATH = "/Graduation/Model/train.log" # This should be in Model, it's right.
 EQUAL_DATA_PATH = "/Graduation/Data/equal.json"
 EQUAL_PARAM_PATH = "/Graduation/Model/equal_param.json"
 EQUAL_TRAIN_REC_PATH = "/Graduation/Model/equal_train_rec.json" # To move
 
 # log file path
+TRAIN_REC_PATH = "/Graduation/Log/train.log" 
 EQUAL_TRAIN_REP_PATH = "/Graduation/Log/equal_train_report.txt"
+EQUAL_TRAIN_TABLE_PATH = "/Graduation/Log/equal_train_table.csv"
 EQUAL_SIF_TRAIN_REC_PATH = "/Graduation/Log/equal_sif_train_rec.json"
 EQUAL_SIF_TRAIN_REP_PATH = "/Graduation/Log/equal_sif_train_rep.json"
 
--- epoch_checkpoint.py
+++ epoch_checkpoint.py
@@ -1,6 +1,7 @@
 import keras
 import logging
 import sys
+import json
 
 class EpochCheckpoint(keras.callbacks.Callback):
     def __init__(self, modelpath, logpath, period):
@@ -11,39 +12,38 @@ class EpochCheckpoint(keras.callbacks.Callback):
         self.logpath = logpath
         self.period = period
 
-        # Set logger
-        self.logger = logging.getLogger("EpochCheckpoint_" + logpath)
-        self.logger.setLevel(logging.DEBUG)
-
-        fh = logging.FileHandler(logpath, mode="w")
-        fh.setLevel(logging.DEBUG)
-        sh = logging.StreamHandler(sys.stdout)
-        sh.setLevel(logging.INFO)
-
-        formatter = logging.Formatter("%(asctime)s-[%(levelname)s] %(message)s")
-        fh.setFormatter(formatter)
-        sh.setFormatter(formatter)
-        self.logger.addHandler(fh)
-        self.logger.addHandler(sh)
+        self.epoch_count = 0
+        self.train_rec = []
 
     def on_train_begin(self, logs={}):
         self.epoch_count = 0
-        self.logger.info("Start Train. Checkpoint will be save at " + self.modelpath)
-        self.logger.info("Detail log file will be saved at " + self.logpath)
+        self.train_rec = [
+            {
+                "model_path": self.modelpath,
+                "save_period": self.period
+            }
+        ]
 
         return super().on_train_begin(logs=logs)
 
     def on_epoch_end(self, epoch, logs={}):
-        self.logger.debug("Epoch " + str(epoch))
         self.epoch_count += 1
+
         if self.epoch_count < self.period:
             return super().on_epoch_end(epoch, logs=logs)
-        
         self.epoch_count = 0
-        self.logger.info("Saving at epoch " + str(epoch) + "...")
 
         # Save checkpoint
         self.model.save(self.modelpath)
-        self.logger.info("Save succeed.")
+
+        # Update record
+        self.train_rec.append(
+            {
+                "epoch": epoch,
+                "logs": logs
+            }
+        )
+        with open(self.logpath, "w") as fd:
+            json.dump(self.train_rec, fd)
             
         return super().on_epoch_end(epoch, logs=logs)
