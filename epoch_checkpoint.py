6fd898083fdf72e08e8a1e554386f3f57bea56a0
supplient <437566830@qq.com>
2020-04-07 09:37:09 +0800

    把指标计算拿走，别在训练时算了……debug麻烦死了

--- epoch_checkpoint.py
+++ epoch_checkpoint.py
@@ -3,6 +3,7 @@ import logging
 import sys
 import json
 import numpy as np
+import os
 
 def getOriginTag(tag):
     if ("B-" in tag) or ("I-" in tag):
@@ -33,37 +34,45 @@ class EpochCheckpoint(keras.callbacks.Callback):
         for key, value in tag_vocab.items():
             self.rev_tag_vocab[value] = key
 
+        # Init train_rec
+        self.train_rec = []
+        if os.path.exists(self.recpath):
+            with open(self.recpath, "r") as fd:
+                self.train_rec = json.load(fd)
+                if self.train_rec[0]["completed"]:
+                    self.train_rec = []
+
         # Init runtime vars
         self.epoch_count = 0
-        self.train_rec = []
-        self.SMALL_CONST = 0.00000001
 
         # Judge each seq's tag
-        self.tags = []
+        self.expect_tags = []
         for tag_id_seq in self.tag_id_seqs:
-            self.tags.append(
+            self.expect_tags.append(
                 judgeWhichTag(tag_id_seq, self.rev_tag_vocab)
             )
-
-        # Count each tag category's expect number
-        self.expect_tag_num = {}
-        for tag in self.tags:
-            num = self.expect_tag_num.get(tag, 0)
-            self.expect_tag_num[tag] = num+1
 
 
     def on_train_begin(self, logs={}):
         self.epoch_count = 0
-        self.train_rec = [
-            {
-                "model_path": self.modelpath,
-                "save_period": self.period,
-                "train_params": self.params,
-                "expect_tag_num": self.expect_tag_num
-            }
-        ]
+        if len(self.train_rec) < 1:
+            self.train_rec = [
+                {
+                    "model_path": self.modelpath,
+                    "save_period": self.period,
+                    "train_params": self.params,
+                    "expect_tags": self.expect_tags,
+                    "completed": False
+                }
+            ]
+        self.updateTrainRec()
 
         return super().on_train_begin(logs=logs)
+
+    def on_train_end(self, logs={}):
+        self.train_rec[0]["completed"] = True
+        self.updateTrainRec()
+        return super().on_train_end(logs=logs)
 
     def on_epoch_end(self, epoch, logs={}):
         self.epoch_count += 1
@@ -89,63 +98,22 @@ class EpochCheckpoint(keras.callbacks.Callback):
             seq = [[np.argmax(x)] for x in one_hot_seq]
             output_seqs.append(seq)
 
-        # Calculate metrics
-        cost_prec, label_prec, output_tag_num = self.cal_metrics(output_seqs)
+        # Judge each output seq's tag
+        output_tags = [judgeWhichTag(seq, self.rev_tag_vocab) for seq in output_seqs]
 
         # Update record
+        print("Updating train record...")
         self.train_rec.append(
             {
                 "epoch": epoch,
                 "logs": logs,
-                "cost_precision": cost_prec,
-                "label_precision": label_prec,
-                "output_tag_num": output_tag_num
+                "output_tags": output_tags
             }
         )
-        with open(self.recpath, "w") as fd:
-            json.dump(self.train_rec, fd, indent=2)
+        self.updateTrainRec()
             
-        print("-cost_precision: %f  -label_precision: %f" % (cost_prec, label_prec))
         return super().on_epoch_end(epoch, logs=logs)
 
-
-    def cal_metrics(self, output_seqs):
-        # Judge each output seq's tag
-        output_tags = [judgeWhichTag(seq, self.rev_tag_vocab) for seq in output_seqs]
-
-        # Cal cost precision
-        ## Count each tag category's actual output num
-        output_tag_num = {}
-        for tag in output_tags:
-            num = output_tag_num.get(tag, 0)
-            output_tag_num[tag] = num + 1
-
-        ## Cal each tag's cost precision
-        cost_p = {}
-        for tag in self.expect_tag_num.keys():
-            if tag == "":
-                continue
-            expect = self.expect_tag_num.get(tag, 0)
-            output = output_tag_num.get(tag, 0)
-            
-            diff = abs(expect - output)
-            if expect == 0:
-                cost_p[tag] = 0
-            else:
-                cost_p[tag] = max(0, expect-diff)/expect
-        print(cost_p)
-
-        ## Cal total cost precision
-        total_cost_p = 1
-        for tag, p in cost_p.items():
-            total_cost_p += p
-        total_cost_p /= len(cost_p)
-
-        # Cal label precision
-        right_count = 0
-        for expect, output in zip(self.tags, output_tags):
-            if expect == output:
-                right_count += 1
-        label_p = right_count/len(output_tags)
-
-        return total_cost_p, label_p, output_tag_num
+    def updateTrainRec(self):
+        with open(self.recpath, "w") as fd:
+            json.dump(self.train_rec, fd, indent=2)
