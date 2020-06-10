cc8a7ada643d404794ab6e7c5474f7c174133290
supplient <437566830@qq.com>
2020-06-10 10:49:39 +0800

    Remove equal concerned config.

--- config.py
+++ config.py
@@ -11,17 +11,10 @@ BERT_CHECKPOINT_PATH = BERT_PATH + "/bert_model.ckpt"
 # generated file path
 TAG_VOCAB_PATH = "/Graduation/Model/tag_vocab.json"
 MODEL_PATH = "/Graduation/Model/model.h5"
-EQUAL_DATA_PATH = "/Graduation/Data/equal.json"
-EQUAL_PARAM_PATH = "/Graduation/Model/equal_param.json"
 
 # log file path
 TRAIN_REC_PATH = "/Graduation/Log/train.json" 
 TRAIN_TABLE_PATH = "/Graduation/Log/train_table.csv"
-EQUAL_TRAIN_REC_PATH = "/Graduation/Log/equal_train_rec.json"
-EQUAL_TRAIN_REP_PATH = "/Graduation/Log/equal_train_report.txt"
-EQUAL_TRAIN_TABLE_PATH = "/Graduation/Log/equal_train_table.csv"
-EQUAL_SIF_TRAIN_REC_PATH = "/Graduation/Log/equal_sif_train_rec.json"
-EQUAL_SIF_TRAIN_REP_PATH = "/Graduation/Log/equal_sif_train_rep.json"
 
 # train & test common config
     # Configs which are specialized 
